b4fe693b311c5e7d78256c73859d3e4dbee6bdf7
ForbiddenR <hostyouposition@outlook.com>
2025-07-15 13:55:25 +0800

    update

--- src/lib.rs
+++ src/lib.rs
@@ -5,29 +5,6 @@ use syn::{Fields, FieldsNamed, FieldsUnnamed, ItemStruct, parse_macro_input, par
 #[proc_macro_attribute]
 pub fn data(_attr: TokenStream, item: TokenStream) -> TokenStream {
     let mut input = parse_macro_input!(item as ItemStruct);
-    input.vis = parse_quote!(pub);
-
-    match &mut input.fields {
-        Fields::Named(FieldsNamed { named, .. }) => {
-            for field in named.iter_mut() {
-                field.vis = parse_quote!(pub);
-            }
-        }
-        Fields::Unnamed(FieldsUnnamed { unnamed, .. }) => {
-            for field in unnamed.iter_mut() {
-                field.vis = parse_quote!(pub);
-            }
-        }
-        Fields::Unit => {}
-    }
-
-    TokenStream::from(quote! {#input})
-}
-
-#[proc_macro_derive(Data)]
-pub fn visible(item: TokenStream) -> TokenStream {
-    let mut input = parse_macro_input!(item as ItemStruct);
-    input.vis = parse_quote!(pub);
 
     match &mut input.fields {
         Fields::Named(FieldsNamed { named, .. }) => {
